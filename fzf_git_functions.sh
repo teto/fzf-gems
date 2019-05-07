5818d6c5b7f43b4554952b03ce4853fea52ed117
Matthieu Coudron <mattator@gmail.com>
2019-05-07 13:10:51 +0900

    Fix Ctrlg CtrlH

--- fzf_git_functions.sh
+++ fzf_git_functions.sh
@@ -41,7 +41,7 @@ function "${FZF_PREFIX}gh" () {
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | xargs git show --color=always | head -'$LINES |
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | xargs git show --color=always | head -' |
   grep -o "[a-f0-9]\{7,\}" | head -1
 }
 
